ff32c19bdfcd35be03c23b18eb0e7ea068a8722c
Ritvik Gupta <108052380+Ritvik2205@users.noreply.github.com>
2023-12-29 00:46:14 +0000

    trivia-commit-4

--- main.js
+++ main.js
@@ -1,16 +1,6 @@
 // Loading the whole DOM content
 document.addEventListener('DOMContentLoaded', function() {
     // checking for correct MCQ answer
-
-    // let sections = document.querySelectorAll('.section');
-    // sections.forEach(function (section) {
-    //     let correct = document.querySelector(CSS.escape(section) + '> .Correct');
-    //     correct.addEventListener('click', function() {
-    //         correct.style.backgroundColor = 'green';
-    //         document.querySelector(CSS.escape(section) + '> .answer').innerHTML = 'Correct!';
-    //     });
-    // });
-
     let corrects = document.querySelectorAll('.Correct');
     for (let i = 0; i < corrects.length; i++) {
         corrects[i].addEventListener('click', function() {
@@ -18,18 +8,6 @@ document.addEventListener('DOMContentLoaded', function() {
             document.querySelector("#answer" + CSS.escape(i+1)).innerHTML = 'Correct!';
         });
     };
-
-    // let correct = document.querySelector('#question1 > .Correct');
-    // correct.addEventListener('click', function() {
-    //     correct.style.backgroundColor = 'green';
-    //     document.querySelector('#answer1').innerHTML = 'Correct!';
-    // });
-
-    // let correct2 = document.querySelector('#question2 > .Correct');
-    // correct2.addEventListener('click', function() {
-    //     correct2.style.backgroundColor = 'green';
-    //     document.querySelector('#answer2').innerHTML = 'Correct!';
-    // });
 
     // checking for incorrect MCQ answer
     let sections = document.querySelectorAll(".section");
@@ -43,32 +21,27 @@ document.addEventListener('DOMContentLoaded', function() {
         };
     };
 
-
-    // let incorrects = document.querySelectorAll("#question1 > .Incorrect");
-    // for (let i = 0; i < incorrects.length; i++) {
-    //     incorrects[i].addEventListener('click', function() {
-    //         incorrects[i].style.backgroundColor = 'red';
-    //         document.querySelector('#answer1').innerHTML = 'Incorrect!';
-    //     })
-    // }
-
-    // let incorrects2 = document.querySelectorAll("#question2 > .Incorrect");
-    // for (let i = 0; i < incorrects2.length; i++) {
-    //     incorrects2[i].addEventListener('click', function() {
-    //         incorrects2[i].style.backgroundColor = 'red';
-    //         document.querySelector('#answer2').innerHTML = 'Incorrect!';
-    //     })
-    // }
-
-    // checking for free response question
+    // checking for free response question - Greek
     document.querySelector('#submit').addEventListener('click', function() {
-    let input = document.querySelector('input');
+    let input = document.querySelector('#input-greek');
     if (input.value == 'Sparta') {
         input.style.backgroundColor = 'green';
         document.querySelector('#answer10').innerHTML = 'Correct!';
     } else {
         input.style.backgroundColor = 'red';
         document.querySelector('#answer10').innerHTML = 'Incorrect!';
+    }
+    });
+
+    // checking for free response question - JJK
+    document.querySelector('#submit').addEventListener('click', function() {
+    let input = document.querySelector('#input-jjk');
+    if (input.value == 'Gauri') {
+        input.style.backgroundColor = 'green';
+        document.querySelector('#answer10-jjk').innerHTML = 'Correct!';
+    } else {
+        input.style.backgroundColor = 'red';
+        document.querySelector('#answer10-jjk').innerHTML = 'Incorrect!';
     }
     });
 });
